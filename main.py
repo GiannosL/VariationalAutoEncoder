a1b9d5fa5d65c5baf81adcf3c7a6920a5db32c1a
GiannosL <giannos.louloudis@gmail.com>
2022-11-22 13:51:01 +0100

    -

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from source.autoencoder.autoencoder import Autoencoder
 # data input
 path_mnist_trainig = "/Users/ljb416/Desktop/projects/datasets/mnist/mnist_train_subset.csv"
 
-# 
+#  
 dataset = Image_Collection(file_path=path_mnist_trainig, x_dimension=28, y_dimension=28)
 dataset.description()
 
 my_autoencoder = Autoencoder(latent_dimensions=10)
 my_autoencoder.train(dataset, 20)
 
-denoised_image = my_autoencoder.transform(dataset.image_tensors[0])
+denoised_image = my_autoencoder.transform(dataset[0])
 denoised_image.show()
