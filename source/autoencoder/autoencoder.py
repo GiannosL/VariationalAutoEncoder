540585800a33148660b608008a2368c34af76a5c
GiannosL <giannos.louloudis@gmail.com>
2022-11-22 08:59:07 +0100

    autoencoder model for use

---
+++ source/autoencoder/autoencoder.py
@@ -0,0 +1,27 @@
+import torch
+from source.autoencoder.raw_autoencoder import Raw_Autoencoder
+from source.data.image_collection import Image_Collection
+from source.data.image import Image
+
+class Autoencoder:
+    def __init__(self, latent_dimensions):
+        self.model = Raw_Autoencoder(latent_dimensions=latent_dimensions)
+
+    def train(self, data: Image_Collection, n_epochs: int):
+        optimizer = torch.optim.Adam(self.model.parameters())
+        for epoch in range(n_epochs):
+            for i in range(0, data.n_images):
+                X = data.image_tensors[i]
+
+                optimizer.zero_grad()
+                # NEED TO MAKE INTO TENSORS
+                X_hat = self.model(X)
+                loss = ((X - X_hat)**2).sum()
+                
+                loss.backward()
+                optimizer.step()
+    
+    def transform(self, x):
+        prediction = self.model.forward(x)
+
+        return Image(prediction.detach().numpy(), 28, 28)
