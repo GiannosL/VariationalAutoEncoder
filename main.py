304970f4112188764a559ceb03446daae7b907fe
GiannosL <giannos.louloudis@gmail.com>
2022-11-23 13:13:24 +0100

    re-structured and commented

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-from source.data.image_collection import Image_Collection
 from source.autoencoder.autoencoder import Autoencoder
-
+from source.data.image_collection import Image_Collection
 
 # data input
-path_mnist_trainig = "/Users/ljb416/Desktop/projects/datasets/mnist/mnist_train_subset.csv"
-
-#  
-dataset = Image_Collection(file_path=path_mnist_trainig, x_dimension=28, y_dimension=28)
+path_mnist_training = "/Users/ljb416/Desktop/projects/datasets/mnist/mnist_train_subset.csv"
+dataset = Image_Collection(file_path=path_mnist_training, x_dimension=28, y_dimension=28)
 dataset.description()
 
+# train autoencoder
 my_autoencoder = Autoencoder(latent_dimensions=10)
 my_autoencoder.train(dataset, 10)
 
-denoised_image = my_autoencoder.transform(dataset.images[0])
+# make predictions
+denoised_image_1 = my_autoencoder.transform(dataset.images[0], dataset.labels[0])
+denoised_image_2 = my_autoencoder.transform(dataset.images[1], dataset.labels[1])
 
-#
+# plot data and results
 dataset.show_image_pca()
 dataset.show_class_distribution()
-denoised_image.show()
+denoised_image_1.show()
+denoised_image_2.show()
