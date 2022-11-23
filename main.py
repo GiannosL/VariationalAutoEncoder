1302e082870c0868b6e3aae27282afecb1f23a08
GiannosL <giannos.louloudis@gmail.com>
2022-11-23 14:00:02 +0100

    plot many prediction's latent space

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from source.data.plot_latent_space import plot_2D_latent
 from source.autoencoder.autoencoder import Autoencoder
 from source.data.image_collection import Image_Collection
 
@@ -7,15 +8,16 @@ dataset = Image_Collection(file_path=path_mnist_training, x_dimension=28, y_dime
 dataset.description()
 
 # train autoencoder
-my_autoencoder = Autoencoder(latent_dimensions=10)
-my_autoencoder.train(dataset, 10)
+my_autoencoder = Autoencoder(latent_dimensions=2)
+my_autoencoder.train(dataset, 100)
 
 # make predictions
-denoised_image_1 = my_autoencoder.transform(dataset.images[0], dataset.labels[0])
-denoised_image_2 = my_autoencoder.transform(dataset.images[1], dataset.labels[1])
+images_list = []
+for i in range(30):
+    img = my_autoencoder.transform(x=dataset.images[i], label=dataset.labels[i])
+    images_list.append(img)
 
 # plot data and results
 dataset.show_image_pca()
 dataset.show_class_distribution()
-denoised_image_1.show()
-denoised_image_2.show()
+plot_2D_latent(image_list=images_list)
