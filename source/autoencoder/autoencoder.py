f63404e555f8d78cd0b28329545b833b849bf948
GiannosL <giannos.louloudis@gmail.com>
2022-11-23 13:07:01 +0100

    show epoch every other time

--- source/autoencoder/autoencoder.py
+++ source/autoencoder/autoencoder.py
@@ -13,32 +13,26 @@ class Autoencoder:
         self.n_epochs = None
 
     def train(self, data: Image_Collection, n_epochs: int):
-        training_loss = []
         optimizer = torch.optim.Adam(self.model.parameters())
         X = torch.FloatTensor(data.images)
 
         for epoch in range(n_epochs):
+            if epoch % 2 == 0:
+                print(f"Running on epoch: {epoch}")
+            
+            for element in X:
 
-            optimizer.zero_grad()
-            
-            X_hat = self.model(X)
-            loss = ((X - X_hat)**2).sum()
-            training_loss.append(loss.item())
-            
-            loss.backward()
-            optimizer.step()
+                optimizer.zero_grad()
+                
+                X_hat = self.model(element)
+                loss = ((element - X_hat)**2).sum()
+                
+                loss.backward()
+                optimizer.step()
         
         self.n_epochs = n_epochs
-        self.training_loss = tuple(training_loss)
     
     def transform(self, x, label="Unknown"):
         x = torch.FloatTensor(x)
         prediction = self.model.forward(x)
         return Image(prediction.detach().numpy(), label, (28, 28))
-    
-    def show_loss_trajectory(self):
-        plt.plot(range(1, self.n_epochs+1), self.training_loss, color="black")
-        plt.title("Loss trajectory during training", weight="bold", fontsize=16)
-        plt.xlabel("Number of epochs", fontsize=12)
-        plt.ylabel("Loss", fontsize=12)
-        plt.show()
