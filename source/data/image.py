88d1b323c41097fd585ac412d528347343f2e126
GiannosL <giannos.louloudis@gmail.com>
2022-11-23 13:59:11 +0100

    commented

--- source/data/image.py
+++ source/data/image.py
@@ -1,15 +1,17 @@
-import torch
 import matplotlib.pyplot as plt
 
 
 class Image:
     def __init__(self, flat_contents, label, 
                  latent_space, dimensions: tuple):
+        # image dimensions
         self.x_dims = dimensions[0]
         self.y_dims = dimensions[1]
 
+        # latent space dimensions, tuple
         self.z = latent_space
-        
+
+        # image contents, flat
         self.contents = flat_contents
         self.label = label
 
