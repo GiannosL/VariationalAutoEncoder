456ce52ea9cba16f105c72049a477c7e3b320aa2
GiannosL <giannos.louloudis@gmail.com>
2022-11-26 13:07:13 +0100

    -

--- source/autoencoder/raw_autoencoder.py
+++ source/autoencoder/raw_autoencoder.py
@@ -3,9 +3,11 @@ import torch.nn as nn
 from source.autoencoder.encoder import Encoder
 from source.autoencoder.decoder import Decoder
 
+
 class Raw_Autoencoder(nn.Module):
     def __init__(self, latent_dimensions):
         super(Raw_Autoencoder, self).__init__()
+
         self.encoder = Encoder(latent_dimensions=latent_dimensions)
         self.decoder = Decoder(latent_dimensions=latent_dimensions)
 
