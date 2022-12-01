0aaebf39d6974cbb3fae65b582a767e422c16a5d
GiannosL <giannos.louloudis@gmail.com>
2022-12-01 18:31:12 +0100

    autoencoder modules

---
+++ source/autoencoder/autoencoder_modules.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Encoder(nn.Module):
+    def __init__(self, latent_dims:int) -> None:
+        super(Encoder, self).__init__()
+        self.linear1 = nn.Linear(784, 512)
+        self.linear2 = nn.Linear(512, latent_dims)
+
+    def forward(self, x):
+        x = torch.flatten(x, start_dim=1)
+        x = F.relu(self.linear1(x))
+        return self.linear2(x)
+
+
+class Decoder(nn.Module):
+    def __init__(self, latent_dims):
+        super(Decoder, self).__init__()
+        self.linear1 = nn.Linear(latent_dims, 512)
+        self.linear2 = nn.Linear(512, 784)
+
+    def forward(self, z):
+        z = F.relu(self.linear1(z))
+        z = torch.sigmoid(self.linear2(z))
+        return z.reshape((-1, 1, 28, 28))
+
+
+class Autoencoder_Model(nn.Module):
+    def __init__(self, latent_dims):
+        super(Autoencoder_Model, self).__init__()
+        self.encoder = Encoder(latent_dims)
+        self.decoder = Decoder(latent_dims)
+
+    def forward(self, x):
+        z = self.encoder(x)
+        return self.decoder(z)
