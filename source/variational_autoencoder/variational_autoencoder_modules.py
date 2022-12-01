3ee6d8878f438dcfdfe372c5d06ffbf8adf48b3d
GiannosL <giannos.louloudis@gmail.com>
2022-12-01 18:43:35 +0100

    added modules for the variational autoencoder

---
+++ source/variational_autoencoder/variational_autoencoder_modules.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from source.autoencoder.autoencoder_modules import Decoder
+
+
+class VariationalEncoder(nn.Module):
+    def __init__(self, latent_dims:int) -> None:
+        super(VariationalEncoder, self).__init__()
+        self.linear1 = nn.Linear(784, 512)
+        self.linear2 = nn.Linear(512, latent_dims)
+        self.linear3 = nn.Linear(512, latent_dims)
+
+        self.N = torch.distributions.Normal(0, 1)
+        self.N.loc = self.N.loc
+        self.N.scale = self.N.scale
+        self.kl = 0
+
+    def forward(self, x):
+        x = torch.flatten(x, start_dim=1)
+        x = F.relu(self.linear1(x))
+        mu =  self.linear2(x)
+        sigma = torch.exp(self.linear3(x))
+        z = mu + sigma*self.N.sample(mu.shape)
+        self.kl = (sigma**2 + mu**2 - torch.log(sigma) - 1/2).sum()
+        return z
+
+
+class Variational_Autoencoder_Model(nn.Module):
+    def __init__(self, latent_dims:int) -> None:
+        super(Variational_Autoencoder_Model, self).__init__()
+        self.encoder = VariationalEncoder(latent_dims)
+        self.decoder = Decoder(latent_dims)
+
+    def forward(self, x):
+        z = self.encoder(x)
+        return self.decoder(z)
