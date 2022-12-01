50e7a6b7e83849b8d5391f820860e90115c72cef
GiannosL <giannos.louloudis@gmail.com>
2022-12-01 19:40:20 +0100

    plot styling

--- source/variational_autoencoder/variational_autoencoder.py
+++ source/variational_autoencoder/variational_autoencoder.py
@@ -15,12 +15,12 @@ class VAE:
     def train(self, data, epochs:int=20, verbose:bool=True) -> None:
         for epoch in range(epochs):
             if (epoch % 5 == 0) and (verbose):
-                print(f"Epoch: {epoch}")
+                print(f"Epoch {epoch}")
             
-            for x in data:
+            for batch, _ in data:
                 self.optimizer.zero_grad()
-                x_hat = self.model(x)
-                loss = self.mse(x, x_hat) + self.model.encoder.kl
+                x_hat = self.model(batch)
+                loss = self.mse(batch, x_hat) + self.model.encoder.kl
                 loss.backward()
                 self.optimizer.step()
         
@@ -30,12 +30,17 @@ class VAE:
     def transform(self, x):
         return self.model(x)
 
-    def plot_latent(self, data, y, num_batches=100) -> None:
-        for i, x in enumerate(data):
-            z = self.model.encoder(x)
+    def plot_latent(self, data, num_batches=100) -> None:
+        plt.figure(figsize=(10, 7))
+        
+        for i, (batch, labels) in enumerate(data):
+            z = self.model.encoder(batch)
             z = z.detach().numpy()
-            plt.scatter(z[:, 0], z[:, 1], c=y[i], cmap='tab10', edgecolors="black")
+            plt.scatter(z[:, 0], z[:, 1], c=labels, cmap='tab10', edgecolors="black")
             if i > num_batches:
                 plt.colorbar()
+                plt.title("Latent space depiction", weight="bold", fontsize=16)
+                plt.xlabel("Dimension 1", fontsize=12)
+                plt.ylabel("Dimension 2", fontsize=12)
                 plt.show()
                 break
